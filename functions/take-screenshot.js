0a60ca7a7461488ac8d1e8a241442c6ec183792b
Eric <EricBandur@gmail.com>
2022-10-03 15:09:34 -0500

    Update js

--- functions/take-screenshot.js
+++ functions/take-screenshot.js
@@ -11,71 +11,32 @@ exports.handler = async (event, context) => {
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`)
-    
-    // setInterval(async function() {
 
-    //     const browser = await chromium.puppeteer.launch({
-    //       args: chromium.args,
-    //       executablePath: process.env.CHROME_EXECUTABLE_PATH || await chromium.executablePath,
-    //       headless: chromium.headless,
-    //     });
-    //     const page = await browser.newPage();
-    //     await page.goto('https://multidollar.company/');
-    //     await page.screenshot({ path: 'currentBeneQueue.png' });
+
+        const browser = await chromium.puppeteer.launch({
+          args: chromium.args,
+          executablePath: process.env.CHROME_EXECUTABLE_PATH || await chromium.executablePath,
+          headless: chromium.headless,
+        });
+        const page = await browser.newPage();
+        await page.goto('https://multidollar.company/');
+        await page.screenshot({ path: 'currentBeneQueue.png' });
       
-    //     await browser.close();
+        await browser.close();
   
-    //     client.channels.cache.get(WHAT_CHANNEL).send("Current Benediction Queue:", {files: ['currentBeneQueue.png']});
-
-    //   console.log('done');
-    // }, 60 * 200);
-  
-  });
-
-  client.on('message', (message) => {
-    if (message.author.bot) return;
-
-    if (message.content === "$q" || message.content === "queue") {
-        (async () => {
-          const browser = await puppeteer.launch();
-          const page = await browser.newPage();
-          await page.goto('https://multidollar.company/');
-          await page.screenshot({ path: 'currentBeneQueue.png' });
-        
-          await browser.close();
-    
-          message.channel.send({files: ['currentBeneQueue.png']});
-        })();
-      }
-
-  })
+        client.channels.cache.get(WHAT_CHANNEL).send("Current Benediction Queue:", {files: ['currentBeneQueue.png']});
 
 
-// exports.handler = async (event, context) => {
-
-    // const pageToScreenshot = JSON.parse(event.body).pageToScreenshot;
-
-    // const browser = await chromium.puppeteer.launch({
-    //     executablePath: await chromium.executablePath,
-    //     args: chromium.args,
-    //     defaultViewport: chromium.defaultViewport,
-    //     headless: chromium.headless,
-    // });
-    
-    // const page = await browser.newPage();
-
-    // await page.goto(pageToScreenshot);
-
-    // const screenshot = await page.screenshot({ encoding: 'binary' });
-
-    // await browser.close();
-  
-    return {
-        statusCode: 200,
-        body: JSON.stringify({ 
-            message: `Complete screenshot of ${pageToScreenshot}`, 
-            buffer: screenshot 
-        })
     }
+
+client.login(process.env.TOKEN)
+
+return {
+    statusCode: 200,
+    body: JSON.stringify({
+      status: 'Ok'
+    })
+  }
+  
 
 }
