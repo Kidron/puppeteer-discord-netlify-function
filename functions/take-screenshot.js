92df3fd679b21157f2aa6b148abd1b7479ce0e4b
Eric <EricBandur@gmail.com>
2022-10-05 13:42:13 -0500

    Update take-screenshot.js

--- functions/take-screenshot.js
+++ functions/take-screenshot.js
@@ -27,24 +27,21 @@ exports.handler = async (event, context) => {
         
             await page.goto(whatSite);
             const screenshot = await page.screenshot();
-            let numberInQueue = await page.$eval('body > section:nth-child(1) > div > h2 > div:nth-child(1)', (el) => el.innerText);
+            let numberInQueue = await page.$eval('body > section:nth-child(1) > div > h2 > div:nth-child(1) > span', (el) => el.innerText);
             numberInQueue = numberInQueue.replace(/\D/g,'');
             numberInQueue = parseInt(numberInQueue);
             console.log(typeof numberInQueue)
-            const blizzETA = await page.$eval('body > section:nth-child(1) > div > h2 > div:nth-child(2)', (el) => el.innerText);
+            const blizzETA = await page.$eval('body > section:nth-child(1) > div > h2 > div:nth-child(2) > span', (el) => el.innerText);
             await browser.close();
 
             if(numberInQueue > 0 || numberInQueue < 15000) {
 
-                await client.channels.cache.get(WHAT_CHANNEL).send(`Number in queue: ${numberInQueue} \n${blizzETA}`, {files: [screenshot]});
+                await client.channels.cache.get(WHAT_CHANNEL).send(`Number in queue: ${numberInQueue} \nBlizzard ETA: ${blizzETA}`, {files: [screenshot]});
 
                 console.log(`Message sent to Discord ${WHAT_CHANNEL}`);
             } else {
                 console.log(`Queue less than 1`);
             }
-
-        
-
 
             return {
                 statusCode: 200,
@@ -55,6 +52,7 @@ exports.handler = async (event, context) => {
 
 
     } catch (error) {
+        await browser.close();
         console.log(error);
         return {
             statusCode: 500,
