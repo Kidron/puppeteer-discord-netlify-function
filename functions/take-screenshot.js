30618cb615544b20926c17d93d2f872df42b2e12
Eric <EricBandur@gmail.com>
2022-10-05 14:03:53 -0500

    Update take-screenshot.js

--- functions/take-screenshot.js
+++ functions/take-screenshot.js
@@ -25,23 +25,23 @@ exports.handler = async (event, context) => {
 
     try {
         
-        await page.goto(whatSite);
-        const screenshot = await page.screenshot();
-        let numberInQueue = await page.$eval('body > section:nth-child(1) > div > h2 > div:nth-child(1)', (el) => el.innerText);
-        numberInQueue = numberInQueue.replace(/\D/g,'');
-        numberInQueue = parseInt(numberInQueue);
-        console.log(typeof numberInQueue)
-        const blizzETA = await page.$eval('body > section:nth-child(1) > div > h2 > div:nth-child(2)', (el) => el.innerText);
-        await browser.close();
+            await page.goto(whatSite);
+            const screenshot = await page.screenshot();
+            let numberInQueue = await page.$eval('body > section:nth-child(1) > div > h2 > div:nth-child(1)', (el) => el.innerText);
+            numberInQueue = numberInQueue.replace(/\D/g,'');
+            numberInQueue = parseInt(numberInQueue);
+            console.log(typeof numberInQueue)
+            const blizzETA = await page.$eval('body > section:nth-child(1) > div > h2 > div:nth-child(2)', (el) => el.innerText);
+            await browser.close();
 
-        if(numberInQueue > 0 || numberInQueue < 15000) {
+            if(numberInQueue > 0 || numberInQueue < 15000) {
 
-            await client.channels.cache.get(WHAT_CHANNEL).send(`Number in queue: ${numberInQueue} \n${blizzETA}`, {files: [screenshot]});
+                await client.channels.cache.get(WHAT_CHANNEL).send(`Number in queue: ${numberInQueue} \n${blizzETA}`, {files: [screenshot]});
 
-            console.log(`Message sent to Discord ${WHAT_CHANNEL}`);
-        } else {
-            console.log(`Queue less than 1`);
-        }
+                console.log(`Message sent to Discord ${WHAT_CHANNEL}`);
+            } else {
+                console.log(`Queue less than 1`);
+            }
 
         
 
