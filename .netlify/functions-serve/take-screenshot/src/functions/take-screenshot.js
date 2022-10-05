9b8ec424612cc4e2c5b0379599ca6a014e29466b
Eric <EricBandur@gmail.com>
2022-10-05 11:53:44 -0500

    Update gitignore and remove netlify/functions module

--- .netlify/functions-serve/take-screenshot/src/functions/take-screenshot.js
+++ .netlify/functions-serve/take-screenshot/src/functions/take-screenshot.js
@@ -1,61 +1,201 @@
-const chromium = require('chrome-aws-lambda');
-const Discord = require('discord.js');
-const client = new Discord.Client();
-const dotenv = require('dotenv');
+var __commonJS = (cb, mod) => function __require() {
+  return mod || (0, cb[Object.keys(cb)[0]])((mod = { exports: {} }).exports, mod), mod.exports;
+};
 
-dotenv.config();
+// node_modules/dotenv/package.json
+var require_package = __commonJS({
+  "node_modules/dotenv/package.json"(exports2, module2) {
+    module2.exports = {
+      name: "dotenv",
+      version: "16.0.3",
+      description: "Loads environment variables from .env file",
+      main: "lib/main.js",
+      types: "lib/main.d.ts",
+      exports: {
+        ".": {
+          require: "./lib/main.js",
+          types: "./lib/main.d.ts",
+          default: "./lib/main.js"
+        },
+        "./config": "./config.js",
+        "./config.js": "./config.js",
+        "./lib/env-options": "./lib/env-options.js",
+        "./lib/env-options.js": "./lib/env-options.js",
+        "./lib/cli-options": "./lib/cli-options.js",
+        "./lib/cli-options.js": "./lib/cli-options.js",
+        "./package.json": "./package.json"
+      },
+      scripts: {
+        "dts-check": "tsc --project tests/types/tsconfig.json",
+        lint: "standard",
+        "lint-readme": "standard-markdown",
+        pretest: "npm run lint && npm run dts-check",
+        test: "tap tests/*.js --100 -Rspec",
+        prerelease: "npm test",
+        release: "standard-version"
+      },
+      repository: {
+        type: "git",
+        url: "git://github.com/motdotla/dotenv.git"
+      },
+      keywords: [
+        "dotenv",
+        "env",
+        ".env",
+        "environment",
+        "variables",
+        "config",
+        "settings"
+      ],
+      readmeFilename: "README.md",
+      license: "BSD-2-Clause",
+      devDependencies: {
+        "@types/node": "^17.0.9",
+        decache: "^4.6.1",
+        dtslint: "^3.7.0",
+        sinon: "^12.0.1",
+        standard: "^16.0.4",
+        "standard-markdown": "^7.1.0",
+        "standard-version": "^9.3.2",
+        tap: "^15.1.6",
+        tar: "^6.1.11",
+        typescript: "^4.5.4"
+      },
+      engines: {
+        node: ">=12"
+      }
+    };
+  }
+});
 
-const WHAT_CHANNEL = process.env.WHAT_CHANNELID;
-const whatSite = 'https://multidollar.company/';
-
-client.on('ready',() => {
-    console.log(`Logged in as ${client.user.tag}!`)
-})
-
-exports.handler = async (event, context) => {
-
-    const browser = await chromium.puppeteer.launch({
-        args: chromium.args,
-        defaultViewport: chromium.defaultViewport,
-        executablePath: process.env.CHROME_EXECUTABLE_PATH || await chromium.executablePath,
-        headless: chromium.headless,
-      });
-
-      const page = await browser.newPage();
-
-    try {
-        
-            await page.goto(whatSite);
-            const screenshot = await page.screenshot();
-            
-            const numberInQueue = await page.$eval('body > section:nth-child(1) > div > h2 > div:nth-child(1) > span', (el) => el.innerText);
-            const blizzETA = await page.$eval('body > section:nth-child(1) > div > h2 > div:nth-child(2) > span', (el) => el.innerText);
-
-            if(numberInQueue === null) {
-                console.log(`Queue less than 1 - DO NOTHING`);
-            } else if(numberInQueue > 0) {
-
-                await client.channels.cache.get(WHAT_CHANNEL).send(`Number in queue: ${numberInQueue} \n Blizzard ETA: ${blizzETA}`, {files: [screenshot]});
-
-                console.log(`Message sent to Discord ${WHAT_CHANNEL}`);
-
-            }
-            await browser.close();
-
-            return {
-                statusCode: 200,
-                body: JSON.stringify({
-                  status: 'Ok'
-                })
-            }
-
-
-    } catch (error) {
-        console.log(error);
-        return {
-            statusCode: 500,
-            body: JSON.stringify({error: 'Failed'}),
+// node_modules/dotenv/lib/main.js
+var require_main = __commonJS({
+  "node_modules/dotenv/lib/main.js"(exports2, module2) {
+    var fs = require("fs");
+    var path = require("path");
+    var os = require("os");
+    var packageJson = require_package();
+    var version = packageJson.version;
+    var LINE = /(?:^|^)\s*(?:export\s+)?([\w.-]+)(?:\s*=\s*?|:\s+?)(\s*'(?:\\'|[^'])*'|\s*"(?:\\"|[^"])*"|\s*`(?:\\`|[^`])*`|[^#\r\n]+)?\s*(?:#.*)?(?:$|$)/mg;
+    function parse(src) {
+      const obj = {};
+      let lines = src.toString();
+      lines = lines.replace(/\r\n?/mg, "\n");
+      let match;
+      while ((match = LINE.exec(lines)) != null) {
+        const key = match[1];
+        let value = match[2] || "";
+        value = value.trim();
+        const maybeQuote = value[0];
+        value = value.replace(/^(['"`])([\s\S]*)\1$/mg, "$2");
+        if (maybeQuote === '"') {
+          value = value.replace(/\\n/g, "\n");
+          value = value.replace(/\\r/g, "\r");
         }
-    }   
-}
+        obj[key] = value;
+      }
+      return obj;
+    }
+    function _log(message) {
+      console.log(`[dotenv@${version}][DEBUG] ${message}`);
+    }
+    function _resolveHome(envPath) {
+      return envPath[0] === "~" ? path.join(os.homedir(), envPath.slice(1)) : envPath;
+    }
+    function config(options) {
+      let dotenvPath = path.resolve(process.cwd(), ".env");
+      let encoding = "utf8";
+      const debug = Boolean(options && options.debug);
+      const override = Boolean(options && options.override);
+      if (options) {
+        if (options.path != null) {
+          dotenvPath = _resolveHome(options.path);
+        }
+        if (options.encoding != null) {
+          encoding = options.encoding;
+        }
+      }
+      try {
+        const parsed = DotenvModule.parse(fs.readFileSync(dotenvPath, { encoding }));
+        Object.keys(parsed).forEach(function(key) {
+          if (!Object.prototype.hasOwnProperty.call(process.env, key)) {
+            process.env[key] = parsed[key];
+          } else {
+            if (override === true) {
+              process.env[key] = parsed[key];
+            }
+            if (debug) {
+              if (override === true) {
+                _log(`"${key}" is already defined in \`process.env\` and WAS overwritten`);
+              } else {
+                _log(`"${key}" is already defined in \`process.env\` and was NOT overwritten`);
+              }
+            }
+          }
+        });
+        return { parsed };
+      } catch (e) {
+        if (debug) {
+          _log(`Failed to load ${dotenvPath} ${e.message}`);
+        }
+        return { error: e };
+      }
+    }
+    var DotenvModule = {
+      config,
+      parse
+    };
+    module2.exports.config = DotenvModule.config;
+    module2.exports.parse = DotenvModule.parse;
+    module2.exports = DotenvModule;
+  }
+});
+
+// functions/take-screenshot.js
+var chromium = require("chrome-aws-lambda");
+var Discord = require("discord.js");
+var client = new Discord.Client();
+var dotenv = require_main();
+dotenv.config();
+var WHAT_CHANNEL = process.env.WHAT_CHANNELID;
+var whatSite = "https://multidollar.company/";
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+exports.handler = async (event, context) => {
+  const browser = await chromium.puppeteer.launch({
+    args: chromium.args,
+    defaultViewport: chromium.defaultViewport,
+    executablePath: process.env.CHROME_EXECUTABLE_PATH || await chromium.executablePath,
+    headless: chromium.headless
+  });
+  const page = await browser.newPage();
+  try {
+    await page.goto(whatSite);
+    const screenshot = await page.screenshot();
+    const numberInQueue = await page.$eval("body > section:nth-child(1) > div > h2 > div:nth-child(1) > span", (el) => el.innerText);
+    const blizzETA = await page.$eval("body > section:nth-child(1) > div > h2 > div:nth-child(2) > span", (el) => el.innerText);
+    if (numberInQueue === null) {
+      console.log(`Queue less than 1 - DO NOTHING`);
+    } else if (numberInQueue > 0) {
+      await client.channels.cache.get(WHAT_CHANNEL).send(`Number in queue: ${numberInQueue} 
+ Blizzard ETA: ${blizzETA}`, { files: [screenshot] });
+      console.log(`Message sent to Discord ${WHAT_CHANNEL}`);
+    }
+    await browser.close();
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        status: "Ok"
+      })
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: "Failed" })
+    };
+  }
+};
 client.login(process.env.TOKEN);
+//# sourceMappingURL=take-screenshot.js.map
